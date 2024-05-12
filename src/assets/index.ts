1217628d1009eb6b1075553cdb64a048dc17486c
Sergio Vargas <sheshovs@gmail.com>
2024-05-11 20:19:33 -0400

    refactor: Update image URLs in assets/index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,6 @@
-import LOGO from './Logo.webp'
-import LOGO_WHITE from './LogoWhite.webp'
-import HEADER_BG from './Dancer.webp'
-import EVENT_BG from './Event.webp'
+const HEADER_BG = `https://images-dantza.s3.us-east-2.amazonaws.com/d1665fc1-5436-4191-b385-d021e6a9bd37.webp`
+const EVENT_BG = `https://images-dantza.s3.us-east-2.amazonaws.com/2467face-e822-4b79-b3ba-3c353adcf3e6.webp`
+const LOGO_WHITE = `https://images-dantza.s3.us-east-2.amazonaws.com/f19863b7-edf0-4820-9082-021fb8ffa1c5.webp`
+const LOGO_COLOR = `https://images-dantza.s3.us-east-2.amazonaws.com/1317af43-182c-47a7-b66a-144248bc7add.webp`
 
-export { LOGO, LOGO_WHITE, HEADER_BG, EVENT_BG }
+export { LOGO_COLOR, LOGO_WHITE, HEADER_BG, EVENT_BG }
